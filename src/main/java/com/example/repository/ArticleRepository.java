8322962f3acaf7c23cbbac59cee444488cfba566
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-28 13:39:34 +0900

    ArticleRepositoryにinsertメソッドを追加

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
@@ -39,5 +41,17 @@ public class ArticleRepository {
 		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
 
 		return articleList;
+	}
+
+	/**
+	 * 記事を挿入する.
+	 * 
+	 * @param name    投稿者名
+	 * @param content 投稿内容
+	 */
+	public void insert(String name, String content) {
+		String sql = "INSERT INTO articles(name, content) VALUES (:name, :content);";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", name).addValue("content", content);
+		template.update(sql, param);
 	}
 }
